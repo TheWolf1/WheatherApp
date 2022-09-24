ee0352c0d20902af0f828f8f9da792d3b5716e43
kevin daniel paz ortiz <pazortizkevindaniel@gmail.com>
2022-09-24 18:05:25 -0500

    primer commit de la app

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios";
+import './App.css'
+import CardWheather from './components/CardWheather'
+
+function App() {
+  const apiKey = "b6338e21c6080d0d84d992e13af47450";
+  const [coords, setCoords] = useState()
+  const [wheather, setWheather] = useState()
+  const [units, setUnits] = useState("metric")
+
+  useEffect(() => {
+   navigator.geolocation.getCurrentPosition((position)=>{
+      setCoords({
+          lat:position.coords.latitude,
+          lon:position.coords.longitude
+      })
+
+      
+  })
+  }, [])
+
+
+  useEffect(() => {
+    if (coords && units) {
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=${units}&lang={af}`
+       axios.get(URL)
+          .then(res=>setWheather(res.data))
+          .catch(err=>console.log(err)) 
+    }
+  }, [coords,units])  
+
+
+  /* Function of change the units */
+  const changeUnits = ()=>{
+    if (units === "metric") {
+      setUnits("imperial")
+    }else{
+      setUnits("metric")
+    }
+  }
+
+console.log(wheather);
+  return (
+    <div className="App">
+      <div className="card-container">
+        <CardWheather wheather={wheather} changeUnits={changeUnits} units={units}/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/CardWheather.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react"
+import axios from "axios";
+
+const CardWheather = ({wheather, changeUnits, units})=>{
+
+    
+    return(
+        <div className="card" id="cardWheater">
+            <div className="card__header">
+                <div className="card__header-title">
+                    <h1> Wheather App  </h1>
+                </div>
+                <div className="card__header-subtitle">
+                    <h3>Ciudad de {wheather?.name}, {wheather?.sys.country}</h3>
+                </div>
+            </div>
+            <div className="card__body">
+                <div className="card__body-col card__body-grades">
+                    <img src={`https://openweathermap.org/img/wn/${wheather?.weather[0].icon}@2x.png`} alt="" />
+                    <span>{wheather?.main.temp}{units=="metric" ? "°C" : "°F" }</span>
+                </div>
+                <div className="card__body-col card__body-info">
+                    <h4>"{wheather?.weather[0].description}"</h4>
+                    <ul>
+                        <li>
+                        <i className="fa-solid fa-wind"></i>
+                            Wind speed: <span>{wheather?.wind.deg} ms</span>
+                        </li>
+                        <li>
+                        <i className="fa-solid fa-cloud"></i>
+                            Clouds: <span>{wheather?.clouds.all}%</span>
+                        </li>
+                        <li>
+                        <i className="fa-solid fa-temperature-three-quarters"></i>
+                            Pressure: <span>{wheather?.main.pressure} mb</span>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="card__footer">
+                <button onClick={changeUnits}>Degrees to {units=="metric" ? "°F" : "°C" }</button>
+            </div>
+        </div>
+    )
+}
+
+
+export default CardWheather
